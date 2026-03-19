0d3c89bbc24218b838b2ccb554a1844eba72f173
jenda <jan.svoboda@11teamsports.com>
2026-03-19 06:21:03 +0100

    env + vue router secured list detail

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import ListDetail from '../views/ListDetail.vue'
 import CreateShoppingList from "@/views/CreateShoppingList.vue";
+import {apiFetch} from "@/serivices/api.ts";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -11,18 +12,21 @@ const router = createRouter({
       name: 'home',
       component: HomeView,
     },
-    // {
-    //   path: '/about',
-    //   name: 'about',
-    //   // route level code-splitting
-    //   // this generates a separate chunk (About.[hash].js) for this route
-    //   // which is lazy-loaded when the route is visited.
-    //   component: ListDetail,
-    // },
     {
       path: '/list/:id',
       name: 'list-detail',
       component: ListDetail,
+      beforeEnter: async (to) => {
+        try {
+          const id = to.params.id
+
+          await apiFetch(`/lists/${id}/items`, { method: 'GET' })
+
+          return true
+        } catch (e) {
+          return { name: 'home' }
+        }
+      }
     },
     {
       path: '/lists/new',
